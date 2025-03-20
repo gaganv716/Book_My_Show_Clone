f16511cbbae2b32bfb9acf11dac7ffa155efabfc
gaganv716 <gaganvijaykumar14@gmail.com>
2025-03-20 19:45:40 +0530

    Added MusicMoviesList with MusicMovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import MovieList from "../components/MovieList";
 import LiveEvents from "../components/LiveEvents";
 import PremieresList from "../components/PremieresList"; // ✅ Import the Premieres component
 import ComedyList from "../components/ComedyList";
+import MusicMoviesList from "../components/MusicMoviesList";
 
 const Home = () => {
   return (
@@ -17,7 +18,6 @@ const Home = () => {
       {/* Now Showing Section */}
       <div className="movielist min-h-screen bg-gray-900 text-white p-6 flex items-center justify-center">
       <div className="container mx-auto px-4 md:px-12 my-8">
-      <h1 className="text-3xl font-bold mb-6">Now Showing</h1>
       <div className="movie-container"></div>
         <MovieList />
       </div>
@@ -27,9 +27,6 @@ const Home = () => {
       {/* The Best of Live Events Section */}
       <div className="bg-gray-800 text-white p-6 mt-8">
       <div className="container mx-auto px-4 md:px-12 my-8">
-        <h2 className="text-3xl font-bold text-center mb-6">
-          The Best Of Live Events
-        </h2>
         <LiveEvents />
       </div>
       </div>
@@ -38,23 +35,25 @@ const Home = () => {
     
       <div className="premiere-background">
       <div className="container mx-auto px-4 md:px-12 my-8">
-        <h2 className="text-3xl font-bold text-center mb-6">
-          Premieres
-        </h2>
-        <p className="text-center text-gray-400 mb-4">
-          Brand new releases every Friday
-        </p>
         <PremieresList />
         </div>
       </div>
 
-      {/*Your Music Studio*/}
+      {/*Your Laughter Capsule*/}
 
       <div className="movielist min-h-screen bg-gray-900 text-white p-6 flex items-center justify-center">
       <div className="container mx-auto px-4 md:px-12 my-8">
-      <h1 className="text-3xl font-bold mb-6">Your Music Studio</h1>
       <div className="movie-container"></div>
         <ComedyList />
+      </div>
+      </div>
+
+       {/*Your Music Studio*/}
+
+       <div className="movielist min-h-screen bg-gray-900 text-white p-6 flex items-center justify-center">
+      <div className="container mx-auto px-4 md:px-12 my-8">
+      <div className="movie-container"></div>
+        <MusicMoviesList />
       </div>
       </div>
     </>
